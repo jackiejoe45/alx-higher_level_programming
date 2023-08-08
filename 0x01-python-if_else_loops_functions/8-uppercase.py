90c916b501b4ae094dba866af60b06d7d09a725f
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-08-08 15:36:30 +0000

    Corrected task 7 and 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            print("{:c}".format(ord(str[i]) - 32), end="")
-        else:
-            print("{:c}".format(ord(str[i])), end="")
+    for char in str:
+        char_code = ord(char)
+        if ord('a') <= char_code <= ord('z'):
+            char_code -= ord('a') - ord('A')
+        print("{}".format(chr(char_code)), end="")
     print("")
