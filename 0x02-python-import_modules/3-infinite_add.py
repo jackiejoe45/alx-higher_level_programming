ea983ea2a05602534c319de7c1b6c01a7863d58a
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-08-11 10:45:05 +0000

    applied py code styles

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import sys
     count = len(sys.argv)
+    sum = 0
     for i in range(1, count):
-        sum += int(sys.argv[i])
+        sum = sum + int(sys.argv[i])
     print("{}".format(sum))
