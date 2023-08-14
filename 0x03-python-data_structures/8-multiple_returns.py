13bcc7cc1623cc96cc7ae02d62f980af14a10132
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-08-14 16:27:01 +0000

    Correced Minor errors

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence != '':
-	return (len(sentence), sentence[0])
-    return (0, None)
+        first_char = sentence[0]
+    else:
+        first_char = None
+    return (len(sentence), first_char)
