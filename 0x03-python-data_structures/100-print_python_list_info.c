fb990c8a68ca9353cae3b78cee244e07943087b7
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-08-14 16:18:22 +0000

    Correced Minor errors

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,6 @@
-import < Python.h >
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
 /*
  * print_python_list_info - prints some basic info about Python lists
  * @p: pointer to PyObject
