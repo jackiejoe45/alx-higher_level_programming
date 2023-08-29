345ca4b8103fc0436f98c00755910b0c6d853b60
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-08-29 19:58:29 +0000

    Corrected Advanced tasks

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,10 +4,9 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Too far")
-            else:
-                result += a ** b / i
-        except (TypeError, ValueError):
+                raise ValueError("Too far")
+            result += a ** b / i
+        except (ValueError):
             result = b + a
             break
     return (result)
