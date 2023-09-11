dc76f91e27378b000780163d103f7c87bf8ac3c0
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-09-11 20:16:55 +0000

    Completed Task Files

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -20,6 +20,7 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
+	self.__size = size
 
     def __str__(self):
         """
