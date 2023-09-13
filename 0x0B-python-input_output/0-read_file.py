8b546d84ba622740d143bdd028bdab90c6e8e3ba
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2023-09-13 15:29:04 +0000

    Completed Task Files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ A function which prints the contents of a file
 
 
 def read_file(filename=""):
+    """
+    Opens the file,file
+    """
     with open(filename) as file:
         content = file.read()
     print(content, end="")
