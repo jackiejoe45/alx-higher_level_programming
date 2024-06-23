ec850e7706cc4a50f25a1a5544953e3444f58c6c
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2024-06-23 22:18:40 +0000

    3

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 import MySQLdb
-import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    ur = sys.argv[1]
+    pd = sys.argv[2]
+    db = sys.argv[3]
 
     try:
         # Connect to MySQL database
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        db = MySQLdb.connect(host="localhost", port=3306, user=ur, passwd=pd, db=db)
         cursor = db.cursor()
 
         # Execute the query to fetch states sorted by states.id
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        
         # Fetch all rows and print them
         rows = cursor.fetchall()
         for row in rows:
@@ -31,4 +29,3 @@ if __name__ == "__main__":
     except MySQLdb.Error as e:
         print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
         sys.exit(1)
-
